c4818c8a30c2456af5d0a5b9c218a071dfeb34c9
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-02-04 15:06:35 -0500

    Created matrix, graph, and geometric operations

--- src/delaunay_tri.c
+++ src/delaunay_tri.c
@@ -3,20 +3,21 @@
  *
  * Delaunay triangulation implementation based on the algorithms presented in
  * "Two Algorithms for Constructing the Delaunay Triangulation," 
- 	International Journal of Computer and Information Science 9(3):219-242, 1980
+	International Journal of Computer and Information Science 9(3):219-242, 1980
  * and
  * "Primitives for the Manipulation of General Subdivisions and the Computation of Voronoi Diagrams," 
- 	ACM Transactions on Graphics 4(2):74-123, April 1985
+	ACM Transactions on Graphics 4(2):74-123, April 1985
  *
  */
 
 #include "delaunay_tri.h"
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DTEPSILON 1e-8 // explore different epsilon values
+#define DTEPSILON 1e-8 // TODO: explore different epsilon values
 
 
 struct vert {
@@ -30,59 +31,210 @@ struct vertNode {
 };
 
 
-int comparePoints(const void *a, const void *b);
+static dtreal XX(const struct vert *v);
+static dtreal YY(const struct vert *v);
+
+int compareVerts(const void *a, const void *b);
+
+static bool ccw(const struct vert *a,
+				const struct vert *b,
+				const struct vert *c);
+static bool rightOf(const struct vert *x, 
+					const struct vert *ea, 
+					const struct vert *eb);
+static bool leftOf(	const struct vert *x,
+					const struct vert *ea,
+					const struct vert *eb);
+static bool inCircle(	const struct vert *a, 
+						const struct vert *b, 
+						const struct vert *c, 
+						const struct vert *d);
+
+static struct vert *first(const struct vert *vi);
+static struct vert *pred(	const struct vert *vi, 
+							const struct vert *vj);
+static struct vert *succ(	const struct vert *vi, 
+							const struct vert *vj);
+static void lct(const struct vert *lrightmost, 
+				const struct vert *rleftmost, 
+				struct vert **lctleft, 
+				struct vert **lctright);
+
 static void ord_dtriangulate(struct vert *v, int ia, int ib);
 
 
 static struct dTriangulation *mtri;
 
 
-// lexicographically compares the points in mtri->points given by indexes a and b.
-int comparePoints(const void *a, const void *b) {
-	int ia = *((int*)a), ib = *((int*)b);
-	dtreal xa = mtri->points[ia*2];
-	dtreal ya = mtri->points[ia*2+1];
-	dtreal xb = mtri->points[ib*2];
-	dtreal yb = mtri->points[ib*2+1];
+static inline dtreal XX(const struct vert *v) {
+	return mtri->points[2 * v->index];
+}
 
-	dtreal diff = xa - xb;
+static inline dtreal YY(const struct vert *v) {
+	return mtri->points[2 * v->index + 1];
+}
+
+
+// lexicographically compares the coordinates of the given vertices
+int compareVerts(const void *a, const void *b) {
+	dtreal diff = XX((const struct vert*)a) - XX((const struct vert*)b);
 
 	if(diff < DTEPSILON && diff > -DTEPSILON) {
-		diff = ya - yb;
+		diff = YY((const struct vert*)a) - YY((const struct vert*)b);
 	}
 
 	return (1 - 2 * signbit(diff));
 }
 
+
+static bool ccw(const struct vert *a,
+				const struct vert *b,
+				const struct vert *c) {
+	dtreal xa = XX(a);
+	dtreal ya = YY(a);
+	dtreal xb = XX(b);
+	dtreal yb = YY(b);
+	dtreal xc = XX(c);
+	dtreal yc = YY(c);
+
+	return (xa * (yb - yc) - ya * (xb - xc) + xb * yc - yb * xc) > 0;
+}
+
+static bool rightOf(const struct vert *x, 
+					const struct vert *ea, 
+					const struct vert *eb) {
+	return ccw(x, eb, ea);
+}
+
+static bool leftOf(	const struct vert *x,
+					const struct vert *ea,
+					const struct vert *eb) {
+	return ccw(x, ea, eb);
+}
+
+// Acknowledgments:
+// Baker, M. (2015). Maths - Matrix algebra - Determinants 4D.
+// http://www.euclideanspace.com/maths/algebra/matrix/functions/determinant/fourD/index.htm
+static bool inCircle(	const struct vert *a, 
+						const struct vert *b, 
+						const struct vert *c, 
+						const struct vert *d) {
+	dtreal m[4][3] = {
+		{XX(a), YY(a)},
+		{XX(b), YY(b)},
+		{XX(c), YY(c)},
+		{XX(d), YY(d)}
+	};
+	m[0][2] = m[0][0] * m[0][0] + m[0][1] * m[0][1];
+	m[1][2] = m[1][0] * m[1][0] + m[1][1] * m[1][1];
+	m[2][2] = m[2][0] * m[2][0] + m[2][1] * m[2][1];
+	m[3][2] = m[3][0] * m[3][0] + m[3][1] * m[3][1];
+
+	return (
+	m[1][2] * m[2][1] * m[3][0] - m[0][2] * m[2][1] * m[3][0] -
+	m[1][1] * m[2][2] * m[3][0] + m[0][1] * m[2][2] * m[3][0] +
+	m[0][2] * m[1][1] * m[3][0] - m[0][1] * m[1][2] * m[3][0] -
+	m[1][2] * m[2][0] * m[3][1] + m[0][2] * m[2][0] * m[3][1] +
+	m[1][0] * m[2][2] * m[3][1] - m[0][0] * m[2][2] * m[3][1] -
+	m[0][2] * m[1][0] * m[3][1] + m[0][0] * m[1][2] * m[3][1] +
+	m[1][1] * m[2][0] * m[3][2] - m[0][1] * m[2][0] * m[3][2] -
+	m[1][0] * m[2][1] * m[3][2] + m[0][0] * m[2][1] * m[3][2] +
+	m[0][1] * m[1][0] * m[3][2] - m[0][0] * m[1][1] * m[3][2] -
+	m[0][2] * m[1][1] * m[2][0] + m[0][1] * m[1][2] * m[2][0] +
+	m[0][2] * m[1][0] * m[2][1] - m[0][0] * m[1][2] * m[2][1] -
+	m[0][1] * m[1][0] * m[2][2] + m[0][0] * m[1][1] * m[2][2]) > 0;
+}
+
+static inline struct vert *first(const struct vert *vi) {
+	if(vi && vi->adj)
+		return vi->adj->v;
+	return NULL;
+}
+
+static struct vert *pred(	const struct vert *vi, 
+							const struct vert *vj) {
+	if(vi && vj) {
+		struct vertNode *vn = vi->adj;
+		if(vn) {
+			do {
+				if(vn->v == vj) {
+					if(vn->prev)
+						return vn->prev->v;
+					return NULL;
+				}
+				vn = vn->prev;
+			} while(vn && vn != vi->adj);
+		}
+	}
+	else {
+		fprintf(stderr, 
+			"Hey silly, you input null vertices in predecessor function!\n");
+	}
+	return NULL;
+}
+
+static struct vert *succ(	const struct vert *vi, 
+							const struct vert *vj) {
+	if(vi && vj) {
+		struct vertNode *vn = vi->adj;
+		if(vn) {
+			do {
+				if(vn->v == vj) {
+					if(vn->next)
+						return vn->next->v;
+					return NULL;
+				}
+				vn = vn->next;
+			} while(vn && vn != vi->adj);
+		}
+	}
+	else {
+		fprintf(stderr, 
+			"Hey silly, you input null vertices in successor function!\n");
+	}
+	return NULL;
+}
+
+static void lct(const struct vert *lrightmost, 
+				const struct vert *rleftmost, 
+				struct vert **lctleft, 
+				struct vert **lctright) {
+	struct vert *za = first(rleftmost);
+	struct vert *zb = first(lrightmost);
+	struct vert *zc = pred(lrightmost, zb);
+	// TODO
+}
+
+
 void dtriangulate(struct dTriangulation *tri) {
 	mtri = tri;
-	int *ind = (int*)malloc(mtri->npoints * sizeof(int));
 
-	for(int i = 0; i < mtri->npoints; ++i) {
-		ind[i] = i;
-	}
-
-	// sort point indexes lexicographically by point coordinates
-	qsort(ind, mtri->npoints, sizeof(int), comparePoints);
-
-	// construct vertex structures of points in sorted order
+	// construct vertex structures of points
 	struct vert *v = (struct vert*)malloc(mtri->npoints * sizeof(struct vert));
 
 	for(int i = 0; i < mtri->npoints; ++i) {
-		v[i].index = ind[i];
+		v[i].index = i;
 		v[i].adj = NULL;
 	}
 
-	free(ind);
+	// sort vertices lexicographically by point coordinates
+	qsort(v, mtri->npoints, sizeof(struct vert), compareVerts);
 
 	// DEBUG
 	// FILE *f = fopen("points.txt", "w");
 
 	// for(int i = 0; i < mtri->npoints; ++i) {
-	// 	fprintf(f, "%f, %f\n", mtri->points[2 * v[i].index], mtri->points[2 * v[i].index + 1]);
+	// 	fprintf(f, "%f, %f\n", XX(&v[i]), YY(&v[i]));
 	// }
 
 	// fclose(f);
+
+	// if(inCircle(&v[0], &v[1], &v[2], &v[3])) {
+	// 	printf("In circle!\n");
+	// }
+	// else {
+	// 	printf("Not in circle!\n");
+	// }
 	//
 
 	// triangulate the sorted points
